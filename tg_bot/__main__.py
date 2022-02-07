52e9a83565679c015b6f3022d0553cf677e12d05
ALARS K MOEDIG <89094414+alarskmoedig@users.noreply.github.com>
2022-02-07 22:22:05 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -20,11 +20,11 @@ from tg_bot.modules.helper_funcs.misc import paginate_modules
 PM_START_TEXT = """
 hoi {}, my name is {}! if you have any questions about how to use me please give me /help... 
 
-im a group manager bot maintained by  [this person](tg://user?id={}).
+im a group manager bot maintained by  [ALARS K MOEDIG](tg://user?id={}).
 
-My future updates will be put into This Channel - @MarieChechi & My Support Group @InFoTelGroup.
+My future updates will be put into My Support Group @alars_chat .
 
-This is my [Deploy Code](https://heroku.com/deploy?template=https://github.com/TGExplore/Marie-2.0-English),
+This is my [Deploy Code](https://youtu.be/x5UDhlqFauo),
 you can create clone same like me..
 
 For more commands click /help...
